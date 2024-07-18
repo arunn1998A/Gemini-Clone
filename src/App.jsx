3a61e19ded087955724c348b365a584830d13ab7
Arun N <arunn5392@gmail.com>
2024-07-18 19:51:55 +0530

    counrt

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+import Main from './components/main/Main'
+// import { Context } from './context/Context'
+
+function App() {
+  return (
+    <>
+ <Sidebar/>
+ <Main/>
+ 
+
+    
+
+
+
+    </>
+  )
+}
+
+export default App
