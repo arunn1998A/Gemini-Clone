8d2ccf1997c0c8372a2f2d37ad24f255398977e8
Arun N <arunn5392@gmail.com>
2024-07-18 20:48:15 +0530

    counrt

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -10,33 +10,36 @@ const ContextProvider = (props) => {
   const [showresult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState('');
-const delayPara =(index,nextWord)=>{
-setTimeout(function(){
-  setResultData(prev=>prev+nextWord)
-  
-}, 75*index);
-}
+
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData(prev => prev + nextWord)
+    }, 75 * index);
+  }
+
   const onSent = async (prompt) => {
     setResultData('')
     setLoading(true)
-    setShowResult((true))
+    setShowResult(true)
     setRecentPrompt(input)
 
-const response=    await run(input);
-let responseArray=response.split("**")
-let newResponse;
-for(let i=0;i<responseArray.length;i++){
-  if(i==0||i%2!==1){
-    newResponse+=responseArray[i]
-  }
-  else{
-    newResponse+="<b>"+responseArray[i]+"</b>"
-  }
-}
-let newResponse2=newResponse2.split("*").join("</br")
-setResultData(newResponse)
-setLoading(false)
-setInput("")
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse = '';
+
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i == 0 || i % 2 !== 1) {
+        newResponse += responseArray[i]
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>"
+      }
+    }
+
+    let newResponse2 = newResponse.split("*").join("</br>");
+
+    setResultData(newResponse2)
+    setLoading(false)
+    setInput("")
   };
 
   const contextValue = {
